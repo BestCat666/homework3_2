c187bbae104adf64ae1262d8cb4c946ed8dc322d
Best Cat <kotova-irina11@rambler.ru>
2022-09-03 22:18:26 +0300

    решение задачи  №13

---
+++ exersice_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите трёхзначное число: ");
+
+int num = int.Parse(Console.ReadLine());                                     
+int FirstDigit = num/100;
+int SecondDigit = (num - FirstDigit * 100) /10; 
+int ThirdDigit = num % 10;
+if (num > 99 && num < 1000)
+{                                  
+Console.WriteLine($"Третья цифра: {ThirdDigit}");
+}
+else
+{
+Console.WriteLine("Вы ввели не трёхзначное число, не балуйтесь!");
+}
+
