57eaff6093a0a364af21aefd01b859eb9bc821b1
Best Cat <kotova-irina11@rambler.ru>
2022-09-03 21:58:22 +0300

    решение задачи  №10

--- exercise_1/Program.cs
+++ exercise_1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трёхзначное число: ");
+
+int num = int.Parse(Console.ReadLine());                                     
+int FirstDigit = num/100;
+int SecondDigit = (num - FirstDigit * 100) /10; 
+int ThirdDigit = (num - FirstDigit * 100 - SecondDigit * 10); // примр для себя,можно было это не писать
+if (num > 99 && num < 1000)
+{                                  
+Console.WriteLine($"Второе число: {SecondDigit}");
+}
+else
+{
+Console.WriteLine("Вы ввели не трёхзначное число, не балуйтесь!");
+}
+
