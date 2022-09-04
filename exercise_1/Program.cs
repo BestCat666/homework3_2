ce285c5bb0f13dfd74e137a230b3dc43da867bd5
Best Cat <kotova-irina11@rambler.ru>
2022-09-04 20:22:44 +0300

    решение задачи  №10

--- exercise_1/Program.cs
+++ exercise_1/Program.cs
@@ -1,4 +1,4 @@
-﻿//  Напишите программу, которая принимает на вход трёхзначное число 
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
 //и на выходе показывает вторую цифру этого числа.
 //456 -> 5
 //782 -> 8
@@ -7,15 +7,30 @@
 Console.WriteLine("Введите трёхзначное число: ");
 
 int num = int.Parse(Console.ReadLine());                                     
-int FirstDigit = num/100;
+int FirstDigit = num/100;                                   
 int SecondDigit = (num - FirstDigit * 100) /10; 
-int ThirdDigit = (num - FirstDigit * 100 - SecondDigit * 10); // или num/10%; примр для себя,можно было это не писать
+int ThirdDigit = (num - FirstDigit * 100 - SecondDigit * 10); // или num/10%; пример для себя,можно было это не писать
 if (num > 99 && num < 1000)
 {                                  
 Console.WriteLine($"Вторая цифра: {SecondDigit}");
 }
 else
 {
-Console.WriteLine("Вы ввели не трёхзначное число, не балуйтесь!");
+Console.WriteLine("Вы ввели не трёхзначное число, не балуйтесь!)");
 }
+
+//Вариант 2
+//Console.WriteLine("Введите трёхзначное число: ");
+//int num = int.Parse(Console.ReadLine());                                     
+//int a = num / 10;                                   
+//int b = a % 10; 
+//if (num > 99 && num < 1000)
+//{                                  
+//Console.WriteLine($"Вторая цифра: {b}");
+//}
+//else
+//{
+//Console.WriteLine("Вы ввели не трёхзначное число, не балуйтесь!");
+//}
+
 
